8299e159744ce5d4d4fe7e64d84655b423693420
Ayanfisco <Ayanfiscoss@gmail.com>
2026-01-28 01:59:35 +0100

    EduCore Management

--- educore_management/__manifest__.py
+++ educore_management/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'EduCore Management System',
+    'name': 'School Management System',
     'version': '18.0.1.0.0',
     'category': 'Education',
     'summary': 'Complete School Management System for Odoo 18',
